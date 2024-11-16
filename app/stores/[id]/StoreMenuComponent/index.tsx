91abe5abd069f2fcd3e6b8e06cffb8c95feb4af7
azhar1598 <mohammedazhar.1598@gmail.com>
2024-11-16 13:33:10 +0530

    fix

--- app/stores/[id]/StoreMenuComponent/index.tsx
+++ app/stores/[id]/StoreMenuComponent/index.tsx
@@ -6,6 +6,7 @@ import MenuCarousel from "./MenuCarousel";
 import Footer from "@/components/common/Footer";
 
 function StoreMenuComponent({ storeDetail }: any) {
+  console.log("storeDD", storeDetail);
   return (
     <Stack>
       <div className=" max-w-[400px] relative w-screen bg-center bg-cover bg-no-repeat  bg-black/70  backdrop-blur-sm">
@@ -13,23 +14,47 @@ function StoreMenuComponent({ storeDetail }: any) {
           src={storeDetail?.websiteTheme?.backgroundImage || "/placeholder.jpg"}
           alt="Store Background"
           fill
+          sizes="full"
           style={{ objectFit: "cover" }}
           priority
         />
         <Stack align="center" pt={20}>
-          <div className="w-16 h-16 rounded-full bg-gray-700 flex items-center justify-center z-[1000]">
-            <span className="text-xl font-bold text-white">
-              {storeDetail?.name.slice(0, 2).toUpperCase()}
-            </span>
+          <div className="w-16 h-16 rounded-full bg-gray-800 flex items-center justify-center z-[1000]">
+            {!storeDetail?.logo ? (
+              <span
+                className={`text-xl font-bold`}
+                style={{ color: storeDetail.websiteTheme.titleColor }}
+              >
+                {" "}
+                {storeDetail?.name.slice(0, 2).toUpperCase()}
+              </span>
+            ) : (
+              <Image
+                src={storeDetail?.logo}
+                alt="logo here"
+                width={100}
+                height={100}
+                style={{ borderRadius: "100px" }}
+                className="rouned-full"
+              />
+            )}
           </div>
 
           <Indicator color="green" position="top-end" processing>
-            <h1 className="text-lg font-bold text-white px-2 ">
+            <h1
+              className={`text-lg font-bold text-white px-2 `}
+              style={{ color: storeDetail.websiteTheme.titleColor }}
+            >
               {storeDetail?.name}
             </h1>
           </Indicator>
 
-          <p className="text-gray-400 -mt-3">{storeDetail?.tagLine}</p>
+          <p
+            className="text-gray-400 -mt-3"
+            style={{ color: storeDetail.websiteTheme.taglineColor }}
+          >
+            {storeDetail?.tagLine}
+          </p>
 
           <MenuCarousel storeDetail={storeDetail} />
           <Footer storeDetail={storeDetail} />
