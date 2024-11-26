de454389f2974436f9522dc32d9cc80ea5106d13
azhar1598 <mohammedazhar.1598@gmail.com>
2024-11-26 22:22:38 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 "use client";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Box, Container, Flex, MantineProvider, Stack } from "@mantine/core";
-import { theme } from "@/theme";
-import { queryClient } from "@/lib/queryClient";
 import "@mantine/core/styles.css";
 import "mantine-datatable/styles.layer.css";
-import { Provider } from "react-redux";
-import store from "@/redux/store";
 import { usePathname } from "next/navigation";
-import "mantine-datatable/styles.layer.css";
-import "@mantine/notifications/styles.css";
-import "@mantine/carousel/styles.css";
 
 import {
   Heebo,
@@ -45,16 +35,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${spaceGrotesk.className} antialiased `}>
         <GoogleAnalytics />
-        <QueryClientProvider client={queryClient}>
-          <MantineProvider theme={theme}>
-            <Notifications position="top-center" />
-            <Provider store={store}>
-              <Flex align={"Flex-center"} justify={"center"} w={"100%"}>
-                <Stack>{children}</Stack>
-              </Flex>
-            </Provider>
-          </MantineProvider>
-        </QueryClientProvider>
+        {children}
       </body>
     </html>
   );
